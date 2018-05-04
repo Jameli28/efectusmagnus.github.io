4cd5c16bb3ba4bc1d1555ced0b00c0a3f19ab359
July Habermann <38544496+efectusmagnus@users.noreply.github.com>
2018-05-04 20:45:42 +0200

    Delete animations.js

--- animations.js
+++
@@ -1,8 +0,0 @@
-function showHide() {
-  var x = document.getElementById("text-box");
-  if (x.style.display === "none") { //if the text is hidden,
-    x.style.display = "block"; //then, show text
-  } else {
-    x.style.display = "none"; //else, hide it.
-  }
-}
